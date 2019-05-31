89cc5afb3042a4ca075d99f7c6f6402f876e2955
prohf <hfhuangfan@gmail.com>
2019-05-31 14:14:43 +0800

    更新load函数

--- Model.py
+++ Model.py
@@ -21,34 +21,31 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 def load_data(file_path, file_name):
     X = pd.read_hdf(file_path+file_name+"_x.hdf", 'data')
     Y = pd.read_hdf(file_path+file_name+"_y.hdf", 'data')
+    X, Y = np.array(X.values), np.array(Y.values).reshape(-1, )
     print(X.shape, Y.shape)
     return X, Y
 
 
-def train_cat(train_data, train_label, test_data, test_label):
-    X, y = np.array(train_data.values), np.array(train_label.values).reshape(-1, )
-    X_test, y_test = np.array(test_data.values), np.array(test_label.values).reshape(-1, )
-    predict = np.zeros(len(X_test))
+def train_cat(x_train, y_train, x_test, y_test):
+    predict = np.zeros(len(x_test))
     # KFold
     kfold = KFold(n_splits=8, shuffle=True)
-    for fold_, (train_id, valid_id) in enumerate(kfold.split(X, y)):
+    for fold_, (train_id, valid_id) in enumerate(kfold.split(x_train, y_train)):
         print(f"fold {fold_}")
-        train_x, train_y = X[train_id], y[train_id]
-        valid_x, valid_y = X[valid_id], y[valid_id]
-        reg = CatBoostRegressor(iterations=20000,
+        train_x, train_y = x_train[train_id], y_train[train_id]
+        valid_x, valid_y = x_train[valid_id], y_train[valid_id]
+        reg = CatBoostRegressor(iterations=200,
                                 depth=2,
                                 learning_rate=1,
                                 loss_function='MAE')
         reg.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)])
-        predict += reg.predict(X_test) / kfold.n_splits
+        predict += reg.predict(x_test) / kfold.n_splits
     mae = mean_absolute_error(predict, y_test)
     return mae
 
 
-def train_lgb(train_data, train_label, test_data, test_label):
-    X, y = np.array(train_data.values), np.array(train_label.values).reshape(-1, )
-    X_test, y_test = np.array(test_data.values), np.array(test_label.values).reshape(-1, )
-    predict = np.zeros(len(X_test))
+def train_lgb(x_train, y_train, x_test, y_test):
+    predict = np.zeros(len(x_test))
     params = {'num_leaves': 51,
               'min_data_in_leaf': 10,
               'objective': 'regression',
@@ -66,15 +63,15 @@ def train_lgb(train_data, train_label, test_data, test_label):
               "random_state": 42}
     # KFold
     kfold = KFold(n_splits=8, shuffle=True)
-    for fold_, (train_id, valid_id) in enumerate(kfold.split(X, y)):
+    for fold_, (train_id, valid_id) in enumerate(kfold.split(x_train, y_train)):
         print(f"fold {fold_}")
-        train_x, train_y = X[train_id], y[train_id]
-        valid_x, valid_y = X[valid_id], y[valid_id]
-        reg = lgb.LGBMRegressor(**params, n_estimators=20000, n_jobs=-1)
+        train_x, train_y = x_train[train_id], y_train[train_id]
+        valid_x, valid_y = x_train[valid_id], y_train[valid_id]
+        reg = lgb.LGBMRegressor(**params, n_estimators=200, n_jobs=-1)
         reg.fit(train_x, train_y,
                 eval_set=[(train_x, train_y), (valid_x, valid_y)],
                 eval_metric='mae')
-        predict += reg.predict(X_test) / kfold.n_splits
+        predict += reg.predict(x_test) / kfold.n_splits
     mae = mean_absolute_error(predict, y_test)
     return mae
 
