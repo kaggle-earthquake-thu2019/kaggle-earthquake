fce91d2f6009625ef7919dc8e4bdf467777eafbf
prohf <hfhuangfan@gmail.com>
2019-06-05 16:05:09 +0800

    当前的特征和模型参数

--- Model.py
+++ Model.py
@@ -1,14 +1,17 @@
 import os
+import sys
 import numpy as np
 import pandas as pd
 import warnings
 from scipy import stats
 from matplotlib import pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import KFold
+import time
 
 from catboost import CatBoostRegressor
 import lightgbm as lgb
@@ -16,71 +19,107 @@ import lightgbm as lgb
 
 warnings.filterwarnings("ignore")
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+n_iter_cat = 2000
+n_iter_lgb = 10000
+n_fold = 16
 
 
 def load_data(file_path, file_name):
     X = pd.read_hdf(file_path+file_name+"_x.hdf", 'data')
     Y = pd.read_hdf(file_path+file_name+"_y.hdf", 'data')
+    features = X.columns.values
     X, Y = np.array(X.values), np.array(Y.values).reshape(-1, )
     print(X.shape, Y.shape)
-    return X, Y
+    return X, Y, features
 
 
-def train_cat(x_train, y_train, x_test, y_test):
-    predict = np.zeros(len(x_test))
-    # KFold
-    kfold = KFold(n_splits=8, shuffle=True)
-    for fold_, (train_id, valid_id) in enumerate(kfold.split(x_train, y_train)):
-        print(f"fold {fold_}")
-        train_x, train_y = x_train[train_id], y_train[train_id]
-        valid_x, valid_y = x_train[valid_id], y_train[valid_id]
-        reg = CatBoostRegressor(iterations=200,
-                                depth=2,
-                                learning_rate=1,
-                                loss_function='MAE')
-        reg.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)])
-        predict += reg.predict(x_test) / kfold.n_splits
-    mae = mean_absolute_error(predict, y_test)
-    return mae
+def load_submission(submission_path, submission_data):
+    submission_x = pd.read_hdf(submission_data)
+    submission = pd.read_csv(submission_path, index_col='seg_id')
+    return submission, np.array(submission_x.values)
 
 
-def train_lgb(x_train, y_train, x_test, y_test):
-    predict = np.zeros(len(x_test))
-    params = {'num_leaves': 51,
+def train_lgb(x_train, y_train, submission_x, features):
+    predict_submission = np.zeros(len(submission_x))
+    feature_importance_df = pd.DataFrame()
+    params = {'num_leaves': 63,
               'min_data_in_leaf': 10,
               'objective': 'regression',
               'max_depth': -1,
-              'learning_rate': 0.001,
+              'learning_rate': 0.01,
               "boosting": "gbdt",
-              "feature_fraction": 0.91,
+              "feature_fraction": 0.9,
               "bagging_freq": 1,
-              "bagging_fraction": 0.91,
-              "bagging_seed": 42,
+              "bagging_fraction": 0.9,
+              "bagging_seed": 3,
               "metric": 'mae',
               "lambda_l1": 0.1,
               "verbosity": -1,
               "nthread": -1,
-              "random_state": 42}
+              "random_state": 30
+              }
     # KFold
-    kfold = KFold(n_splits=8, shuffle=True)
+    kfold = KFold(n_splits=n_fold, shuffle=True)
+    mae = 0
     for fold_, (train_id, valid_id) in enumerate(kfold.split(x_train, y_train)):
         print(f"fold {fold_}")
         train_x, train_y = x_train[train_id], y_train[train_id]
         valid_x, valid_y = x_train[valid_id], y_train[valid_id]
-        reg = lgb.LGBMRegressor(**params, n_estimators=200, n_jobs=-1)
+        reg = lgb.LGBMRegressor(**params, n_estimators=n_iter_lgb)
         reg.fit(train_x, train_y,
                 eval_set=[(train_x, train_y), (valid_x, valid_y)],
-                eval_metric='mae')
-        predict += reg.predict(x_test) / kfold.n_splits
-    mae = mean_absolute_error(predict, y_test)
-    return mae
+                eval_metric='mae',
+                early_stopping_rounds=1500)
+
+        predict_submission += reg.predict(submission_x) / kfold.n_splits
+        predict_valid = reg.predict(valid_x, num_iteration=reg.best_iteration_)
+        mae += mean_absolute_error(predict_valid, valid_y) / kfold.n_splits
+        # count features importance
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["Feature"] = features
+        fold_importance_df["importance"] = reg.feature_importances_[:len(features)]
+        fold_importance_df["fold"] = fold_ + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+    return mae, predict_submission, feature_importance_df
+
+
+def features_importance(feature_importance_df, timestamp):
+    cols = (feature_importance_df[["Feature", "importance"]]
+            .groupby("Feature")
+            .mean()
+            .sort_values(by="importance", ascending=False)[:-1].index)
+    best_features = feature_importance_df.loc[feature_importance_df.Feature.isin(cols)]
+
+    plt.figure(figsize=(14, 26))
+    sns.barplot(x="importance", y="Feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (averaged over folds)')
+    plt.tight_layout()
+    plt.savefig('../output/' + timestamp + '_lgbm_importances.png')
 
 
 if __name__ == '__main__':
     file_dir = '../train/'
-    train_data, train_label = load_data(file_dir, "train")
-    test_data, test_label = load_data(file_dir, "1")
-    cat_mae = train_cat(train_data, train_label, test_data, test_label)
-    lgb_mae = train_lgb(train_data, train_label, test_data, test_label)
-    print("catboost mae: ", cat_mae)
-    print("lightGBM mae: ", lgb_mae)
+    output_dir = '../output/'
+    train_data, train_label, features = load_data(file_dir, "train")
+
+    # submission
+    submission_df, submission_x = load_submission(file_dir + "sample_submission.csv", file_dir + "submission.hdf")
+
+    # train and predict
+    lgb_start = time.time()
+    lgb_mae, submission_ttf, feature_df = train_lgb(train_data, train_label, submission_x, features)
+    lgb_end = time.time()
+
+    # save submission
+    timestamp = str(time.time()).split(".")[0]
+    output_txt = output_dir + timestamp + '_result.txt'
+    submission_df['time_to_failure'] = submission_ttf
+    submission_df.to_csv(output_dir + timestamp + '_submission.csv', index=True)
+
+    # save result
+    with open(output_txt, 'w+') as output:
+        print(f"LightGBM mae: {lgb_mae}, use {lgb_end - lgb_start}s", file=output)
+        print(f"LightGBM mae: {lgb_mae}, use {lgb_end - lgb_start}s")
+
+    # show feature importance
+    features_importance(feature_df, timestamp)
--- feature_generator.py
+++ feature_generator.py
@@ -3,13 +3,10 @@ import numpy as np
 import pandas as pd
 import warnings
 from scipy import stats
+from scipy import signal
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import LeaveOneOut
-from sklearn.model_selection import KFold
-
 
 warnings.filterwarnings("ignore")
 
@@ -22,19 +19,150 @@ def load_data(file_path):
     return signal_data
 
 
+def get_spectrum(input_signal, window=100):
+    """
+    returns the Fourier power spectrum for a given signal segment
+    output is a pandas Series
+    output.index is the frequencies
+    output.values is the amplitudes for each frequencies
+    default moving average window is 10
+    """
+    sampling_frequency = np.ceil(150000 / 0.0375)
+    input_signal = np.asarray(input_signal, dtype='float32')
+
+    # Remove the mean
+    input_signal -= input_signal.mean()
+
+    # Estimate power spectral density using a periodogram.
+    frequencies, power_spectrum = signal.periodogram(
+        input_signal, sampling_frequency, scaling='spectrum')
+
+    # Run a running windows average of 10-points to smooth the signal (default).
+    power_spectrum = pd.Series(power_spectrum, index=frequencies).rolling(window=window).mean()
+
+    return pd.Series(power_spectrum)
+
+
 def generate_features(seg_id, segment, X):
     xc = pd.Series(segment)
     zc = np.fft.fft(xc)
 
+    # time domain
     X.loc[seg_id, 'mean'] = xc.mean()
-    X.loc[seg_id, 'std'] = xc.std()
+    # X.loc[seg_id, 'std'] = xc.std()
     X.loc[seg_id, 'max'] = xc.max()
     X.loc[seg_id, 'min'] = xc.min()
+    X.loc[seg_id, 'autocorr'] = xc.autocorr()
 
     X.loc[seg_id, 'mad'] = xc.mad()
     X.loc[seg_id, 'kurt'] = xc.kurtosis()
     X.loc[seg_id, 'skew'] = xc.skew()
-    X.loc[seg_id, 'med'] = xc.median()
+    # X.loc[seg_id, 'med'] = xc.median()
+
+    # X.loc[seg_id, 'sem'] = xc.sem()
+    # X.loc[seg_id, 'var'] = xc.var()
+
+    X.loc[seg_id, 'diff_ave'] = xc.diff().mean()
+    X.loc[seg_id, 'diff_max'] = xc.diff().max()
+    X.loc[seg_id, 'diff_mad'] = xc.diff().mad()
+    X.loc[seg_id, 'diff_kurt'] = xc.diff().kurtosis()
+    X.loc[seg_id, 'diff_skew'] = xc.diff().skew()
+
+    # first last 50000
+    for window in [50000, 10000, 5000]:
+        X.loc[seg_id, 'mean_first' + str(window)] = xc[:window].mean()
+        # X.loc[seg_id, 'std_first' + str(window)] = xc[:window].std()
+        X.loc[seg_id, 'autocorr_first' + str(window)] = xc[:window].autocorr()
+        X.loc[seg_id, 'max_first' + str(window)] = xc[:window].max()
+        # X.loc[seg_id, 'med_first' + str(window)] = xc[:window].median()
+        # X.loc[seg_id, 'min_first' + str(window)] = xc[:window].min()
+        X.loc[seg_id, 'mad_first' + str(window)] = xc[:window].mad()
+        X.loc[seg_id, 'kurt_first' + str(window)] = xc[:window].kurtosis()
+        X.loc[seg_id, 'skew_first' + str(window)] = xc[:window].skew()
+
+        X.loc[seg_id, 'mean_last' + str(window)] = xc[-window:].mean()
+        # X.loc[seg_id, 'std_last' + str(window)] = xc[-window:].std()
+        X.loc[seg_id, 'autocorr_last' + str(window)] = xc[-window:].autocorr()
+        X.loc[seg_id, 'max_last' + str(window)] = xc[-window:].max()
+        # X.loc[seg_id, 'med_last' + str(window)] = xc[-window:].median()
+        # X.loc[seg_id, 'min_last' + str(window)] = xc[-window:].min()
+        X.loc[seg_id, 'mad_last' + str(window)] = xc[-window:].mad()
+        X.loc[seg_id, 'kurt_last' + str(window)] = xc[-window:].kurtosis()
+        X.loc[seg_id, 'skew_last' + str(window)] = xc[-window:].skew()
+
+    # absolute
+    abs_xc = pd.Series(np.abs(xc))
+    X.loc[seg_id, 'abs_mean'] = abs_xc.mean()
+
+    # frequency domain
+    realFFT = pd.Series(np.real(zc))
+    imagFFT = pd.Series(np.imag(zc))
+    X.loc[seg_id, 'r_mean'] = realFFT.mean()
+    # X.loc[seg_id, 'r_std'] = realFFT.std()
+    X.loc[seg_id, 'r_max'] = realFFT.max()
+    # X.loc[seg_id, 'r_min'] = realFFT.min()
+
+    X.loc[seg_id, 'i_mean'] = imagFFT.mean()
+    # X.loc[seg_id, 'i_std'] = imagFFT.std()
+    X.loc[seg_id, 'i_max'] = imagFFT.max()
+    # X.loc[seg_id, 'i_min'] = imagFFT.min()
+
+    X.loc[seg_id, 'r_mad'] = realFFT.mad()
+    X.loc[seg_id, 'r_kurt'] = realFFT.kurtosis()
+    X.loc[seg_id, 'r_skew'] = realFFT.skew()
+    X.loc[seg_id, 'r_var'] = realFFT.var()
+    X.loc[seg_id, 'r_sem'] = realFFT.sem()
+
+    X.loc[seg_id, 'i_mad'] = imagFFT.mad()
+    X.loc[seg_id, 'i_kurt'] = imagFFT.kurtosis()
+    X.loc[seg_id, 'i_skew'] = imagFFT.skew()
+    # X.loc[seg_id, 'i_var'] = imagFFT.var()
+    # X.loc[seg_id, 'i_sem'] = imagFFT.sem()
+
+    # quantile
+    X.loc[seg_id, 'q95'] = np.quantile(xc, 0.95)
+    X.loc[seg_id, 'q91'] = np.quantile(xc, 0.91)
+    X.loc[seg_id, 'q99'] = np.quantile(xc, 0.99)
+
+    X.loc[seg_id, 'q01'] = np.quantile(xc, 0.01)
+    X.loc[seg_id, 'q05'] = np.quantile(xc, 0.05)
+    X.loc[seg_id, 'q09'] = np.quantile(xc, 0.09)
+
+    # # rolling windows
+    for window in [3000, 5000, 10000]:
+        roll_mean = pd.Series(xc.rolling(window=window).mean().dropna().values)
+        zc_rolling = np.fft.fft(xc.rolling(window=window).mean().dropna().values)
+        # roll_mean_realfft = pd.Series(np.real(zc_rolling))
+        roll_mean_imagfft = pd.Series(np.imag(zc_rolling))
+
+        X.loc[seg_id, 'roll_mean_mad_' + str(window)] = roll_mean.mad()
+        # X.loc[seg_id, 'roll_mean_kurt_' + str(window)] = roll_mean.kurtosis()
+        X.loc[seg_id, 'roll_mean_skew_' + str(window)] = roll_mean.skew()
+
+        # X.loc[seg_id, 'roll_mean_r_mean' + str(window)] = roll_mean_realfft.mean()
+        # X.loc[seg_id, 'roll_mean_r_max' + str(window)] = roll_mean_realfft.max()
+
+        X.loc[seg_id, 'roll_mean_i_mean' + str(window)] = roll_mean_imagfft.mean()
+        X.loc[seg_id, 'roll_mean_i_max' + str(window)] = roll_mean_imagfft.max()
+        X.loc[seg_id, 'roll_mean_i_skew' + str(window)] = roll_mean_imagfft.skew()
+        # X.loc[seg_id, 'roll_mean_i_kurt' + str(window)] = roll_mean_imagfft.kurtosis()
+
+    # power spectrum
+    max_frequency = 300000
+    min_frequency = 100
+    power_spectrum = get_spectrum(segment, window=10).dropna()
+    power_spectrum = power_spectrum[power_spectrum.index < max_frequency]
+    power_spectrum = power_spectrum[power_spectrum.index > min_frequency]
+
+    X.loc[seg_id, 'spectrum_mean'] = power_spectrum.mean()
+    X.loc[seg_id, 'spectrum_std'] = power_spectrum.std()
+    X.loc[seg_id, 'spectrum_max'] = power_spectrum.max()
+    X.loc[seg_id, 'spectrum_min'] = power_spectrum.min()
+    X.loc[seg_id, 'spectrum_med'] = power_spectrum.median()
+    X.loc[seg_id, 'spectrum_skew'] = power_spectrum.skew()
+    X.loc[seg_id, 'spectrum_kurt'] = power_spectrum.kurtosis()
+    X.loc[seg_id, 'spectrum_mad'] = power_spectrum.mad()
+
 
 
 def data_process(file_path):
@@ -90,8 +218,26 @@ def make_dataset(file_dir, output_dir):
     Y.to_hdf(output_dir + "train_y.hdf", 'data')
 
 
+def make_submission(submission_dir, submission_file, output_dir):
+    submission = pd.read_csv(submission_file, index_col='seg_id')
+    submission_x = pd.DataFrame(dtype=np.float64, index=submission.index)
+    for seg_id in tqdm(submission_x.index):
+        test_df = pd.read_csv(submission_dir + seg_id + '.csv', dtype={'acoustic_data': np.int16})
+        segment = test_df['acoustic_data'].values
+        generate_features(seg_id, segment, submission_x)
+    # scale
+    scaler = StandardScaler()
+    scaler.fit(submission_x)
+    submission_x = pd.DataFrame(scaler.transform(submission_x), columns=submission_x.columns)
+    # save
+    submission_x.to_hdf(output_dir + "submission.hdf", 'data')
+
+
 if __name__ == '__main__':
     file_dir = '../earthquakes/'
     output_dir = '../train/'
+    submission_path = '../test/'
+    submission_file = '../test/sample_submission.csv'
     if not computer_feature:
         make_dataset(file_dir, output_dir)
+    make_submission(submission_path, submission_file, output_dir)
